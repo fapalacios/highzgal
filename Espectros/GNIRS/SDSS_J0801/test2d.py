92898d2329a272740ad7509055750603c12a8539
felicia <felicia.palacios@gmail.com>
2019-09-08 13:51:42 -0300

    bpt codes and quality of lines

--- Espectros/GNIRS/SDSS_J0801/test2d.py
+++ Espectros/GNIRS/SDSS_J0801/test2d.py
@@ -10,21 +10,19 @@ from astropy.io import fits
 import numpy as np
 from matplotlib import pyplot as plt
 
-hdu_list = fits.open('J2221_SCI_SLIT_FLUX_MERGE2D_NIR.fits')
+hdu_list = fits.open('j2_2d.fits')
 
-image_data = hdu_list[0].data
-<<<<<<< HEAD:Espectros/Xshooter_arcs/J2221/test2d.py
-header = hdu_list[0].header
+image_data = hdu_list[1].data
+header = hdu_list[1].header
 l_0 = header['CRVAL1']
 step = header['CDELT1']
 
 
-x =  np.arange(0, len(image_data[0]))*step*10 + l_0*10
+x =  np.arange(0, len(image_data[0]))*step + l_0
 x_pos = np.arange(0, len(x), 5)
 x_label = x[: : 10]
-=======
->>>>>>> b2ab684f36922f5ecde738e339c88edb3c9f1b8b:Espectros/GNIRS/SDSS_J0801/test2d.py
 
 plt.imshow(image_data, vmin=-3.118e-18, vmax=1.548e-18)
-plt.xticks(x_pos, x_label)
-plt.xlim(16772 - 50, 16772 + 50)
+plt.ylim(400, 450)
+#plt.xticks(x_pos, x_label)
+#plt.xlim(16772 - 50, 16772 + 50)
---
+++ bpt.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#J2221
+plt.scatter(-1.1929410082494034, 0.42539790997307675, color='black')
+plt.arrow(-1.1929410082494034, 0.42539790997307675, 0, 0.05, width=0.02, color='black')
+#J021247
+plt.scatter(0.0, 0.8064502355915049, color='black')
+plt.arrow(0.0, 0.8064502355915049, 0, 0.05, width=0.02, color='black')
+plt.arrow(0.0, 0.8064502355915049, -0.05, 0, width=0.02, color='black')
+#J023251
+plt.scatter(-1.2335265181181607, 0.7864913406584415, color='black')
+#J084959
+plt.scatter(-0.5549430492841291, 0.8488002595502193, color='black')
+plt.arrow(-0.5549430492841291, 0.8488002595502193, 0, 0.05, width=0.02, color='black')
+plt.arrow(-0.5549430492841291, 0.8488002595502193, -0.05, 0, width=0.02, color='black')
+#J090407
+plt.scatter(-0.758054161352852, 0.7690785646287618, color='black')
+#J095921
+plt.scatter(-2, -0.17521629375982367, color='black')
+plt.arrow(-2, -0.17521629375982367, 0, 0.05, width=0.02, color='black')
+#J221326
+plt.scatter(-2, 1.4908240358852602, color='black')
+plt.arrow(-2, 1.4908240358852602, 0, 0.05, width=0.02, color='black')
+
+x1 = np.linspace(-2, 0.25)
+
+plt.plot(x1 , 0.61/(x1 - 0.02 - 0.1833*2.5 )+ 1.2 + 0.03*2.5)
+plt.xlim(-2.01, 1)
+plt.ylim(-1, 1.8)
+plt.title('z ~ 2.5')
+plt.xlabel('LOG([NII]/Ha)')
+plt.ylabel('LOG([OIII]/Hb)')
+plt.savefig('BPT.png')
+plt.show()
